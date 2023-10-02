68d47228ddbcc23ba25464ac226a4a10a8ac603d
Qu1etboy <nonzagreanthai@gmail.com>
2023-10-03 01:38:40 +0700

    feat(overview): display real data

--- apps/web/src/app/(web)/seller/[storeId]/page.tsx
+++ apps/web/src/app/(web)/seller/[storeId]/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Overview from "../components/overview";
 import { AreaChart, DollarSign, Package, Users2 } from "lucide-react";
 import MainLayout from "@/components/layout/main-layout";
+import { prisma } from "@/lib/prisma";
+import Currency from "@/components/currency";
 
 const data = [
   {
@@ -19,7 +21,58 @@ const data = [
   },
 ];
 
-export default function Page() {
+export default async function Page({
+  params,
+}: {
+  params: { storeId: string };
+}) {
+  const review = await prisma.review.count({
+    where: {
+      product: {
+        storeId: params.storeId,
+      },
+    },
+  });
+
+  const orders = await prisma.order.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  const totalSales = await prisma.orderItem.aggregate({
+    where: {
+      orderId: { in: orders.map((order) => order.id) },
+    },
+    _sum: {
+      quantity: true,
+    },
+  });
+
+  const totalAmount = await prisma.receipt.aggregate({
+    where: {
+      bill: {
+        order: {
+          some: {
+            id: { in: orders.map((order) => order.id) },
+          },
+        },
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const product = await prisma.product.aggregate({
+    where: {
+      storeId: params.storeId,
+    },
+    _sum: {
+      stockQuantity: true,
+    },
+  });
+
   return (
     <MainLayout title="ภาพรวม" description="ภาพรวมร้านค้าของคุณ">
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
@@ -29,7 +82,9 @@ export default function Page() {
             <DollarSign className="w-4 h-4" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">฿45,231.89</div>
+            <div className="text-2xl font-bold">
+              <Currency value={totalAmount._sum.amount || 0} />{" "}
+            </div>
             <p className="text-xs text-muted-foreground">
               +20.1% จากเดือนที่แล้ว
             </p>
@@ -41,7 +96,7 @@ export default function Page() {
             <AreaChart className="w-4 h-4" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+2350</div>
+            <div className="text-2xl font-bold">{totalSales._sum.quantity}</div>
             <p className="text-xs text-muted-foreground">
               +180.1% จากเดือนที่แล้ว
             </p>
@@ -53,7 +108,7 @@ export default function Page() {
             <Users2 className="w-4 h-4" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
+            <div className="text-2xl font-bold">{review}</div>
             <p className="text-xs text-muted-foreground">
               +19% จากเดือนที่แล้ว
             </p>
@@ -65,7 +120,9 @@ export default function Page() {
             <Package className="w-4 h-4" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+573</div>
+            <div className="text-2xl font-bold">
+              {product._sum.stockQuantity}
+            </div>
             <p className="text-xs text-muted-foreground">
               -201 เมื่อ 2 สัปดาห์ที่แล้ว
             </p>
