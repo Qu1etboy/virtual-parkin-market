518ec35d6d6b2965ea324b30692e1af32cde0640
Qu1etboy <nonzagreanthai@gmail.com>
2023-10-05 20:51:48 +0700

    feat: protected seller route only store owner can view their store

--- apps/web/src/app/(web)/seller/[storeId]/layout.tsx
+++ apps/web/src/app/(web)/seller/[storeId]/layout.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import SellerNavbar from "../components/navbar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/auth-options";
+import { prisma } from "@/lib/prisma";
+import { notFound, redirect } from "next/navigation";
 
-export default function SellerLayout({
+export default async function SellerLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: { storeId: string };
 }) {
+  const session = await getServerSession(authOptions);
+  const store = await prisma.store.findUnique({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  if (!store) {
+    notFound();
+  }
+
+  if (store?.userId !== session?.user?.id) {
+    redirect("/");
+  }
+
   return (
     <div>
       <SellerNavbar storeId={params.storeId} />
