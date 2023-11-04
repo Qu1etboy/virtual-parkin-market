43f4b036800056400e20774666ff405a650dacc9
Qu1etboy <nonzagreanthai@gmail.com>
2023-11-04 22:44:00 +0700

    feat: write terms and conditions, privacy & shipping  policy, about us

--- apps/web/src/app/(web)/(shop)/(info)/about/page.tsx
+++ apps/web/src/app/(web)/(shop)/(info)/about/page.tsx
@@ -1,16 +1,19 @@
-import { ArrowRightCircleIcon } from "lucide-react";
 import React from "react";
 import InfoLayout from "../info-layout";
 
 export default function AboutPage() {
   return (
-    <InfoLayout title="เกี่ยวกับเรา">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nesciunt
-        culpa unde totam obcaecati, cupiditate doloribus? Et excepturi dolor
-        deserunt accusantium eum minima officiis, asperiores accusamus
-        consequatur placeat non dignissimos?
-      </p>
-    </InfoLayout>
+    <>
+      <div className="w-full h-[300px]">
+        <img
+          src="https://img.wongnai.com/p/1920x0/2020/08/08/6d834add42204a3fb991dff27a819cbe.jpg"
+          alt="Park In Market banner"
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+      <InfoLayout title="เกี่ยวกับเรา">
+        <p>ตลาดสุดชิคใจกลางบางแสนในบรรยากาศสวนสวย</p>
+      </InfoLayout>
+    </>
   );
 }
